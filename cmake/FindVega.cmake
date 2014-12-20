b14030e9903544a98ac841b8d831b015577b1cbc
ArikatlaRPI <venkatnitk@gmail.com>
2014-12-20 15:25:13 -0500

    Modified some cmake files to include vega!

---
+++ cmake/FindVega.cmake
@@ -0,0 +1,18 @@
+find_path(VEGA_INCLUDE_DIR
+  forceModel/forceModel.h)
+find_library(VEGA_LIBRARY
+  NAMES
+    vega)
+
+set(AUDIERE_INCLUDE_DIRS "${VEGA_INCLUDE_DIR}")
+set(AUDIERE_LIBRARIES "${VEGA_LIBRARY}")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(vega
+  REQUIRED_VARS
+    VEGA_INCLUDE_DIR
+    VEGA_LIBRARY)
+
+mark_as_advanced(
+  VEGA_INCLUDE_DIR
+  VEGA_LIBRARY)
