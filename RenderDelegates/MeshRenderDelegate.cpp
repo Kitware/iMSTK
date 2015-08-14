8c38a0e6edd755dddb6b173f9f1133cb362e7e5e
Ricardo Ortiz <ricardo.ortiz@kitware.com>
2015-08-14 08:18:35 -0400

    Format according to specs...

--- RenderDelegates/MeshRenderDelegate.cpp
+++ RenderDelegates/MeshRenderDelegate.cpp
@@ -24,43 +24,46 @@
 #include "Core/Geometry.h"
 #include "Core/RenderDelegate.h"
 #include "Core/Factory.h"
-#include "Core/BaseMesh.h"
+#include "Mesh/SurfaceMesh.h"
 
 class MeshRenderDelegate : public RenderDelegate
 {
 public:
-  virtual void draw() const override;
-  virtual bool isTargetTextured() const override;
+    virtual void draw() const override;
+    virtual bool isTargetTextured() const override;
 };
 
 void MeshRenderDelegate::draw() const
 {
-  auto geom = this->getSourceGeometryAs<Mesh>();
-  if (!geom)
-    return;
-
-  auto mesh = std::dynamic_pointer_cast<Mesh>(geom->shared_from_this());
-  OpenGLRenderer::drawSurfaceMeshTriangles(mesh, geom->getRenderDetail());
-
-  if (geom->getRenderDetail()->renderType & SIMMEDTK_RENDER_NORMALS)
+    auto geom = this->getSourceGeometryAs<SurfaceMesh>();
+    if(!geom)
     {
-    OpenGLRenderer::drawNormals(mesh,
-      geom->getRenderDetail()->normalColor,
-      geom->getRenderDetail()->normalLength);
+        return;
+    }
+
+    auto mesh = std::static_pointer_cast<SurfaceMesh>(geom->shared_from_this());
+    OpenGLRenderer::drawSurfaceMeshTriangles(mesh, geom->getRenderDetail());
+
+    if(geom->getRenderDetail()->renderType & SIMMEDTK_RENDER_NORMALS)
+    {
+        OpenGLRenderer::drawNormals(mesh,
+                                    geom->getRenderDetail()->normalColor,
+                                    geom->getRenderDetail()->normalLength);
     }
 }
 
 bool MeshRenderDelegate::isTargetTextured() const
 {
-  auto geom = this->getSourceGeometryAs<Mesh>();
-  if (!geom)
-    return false;
-
-  return geom->isMeshTextured();
+    auto geom = this->getSourceGeometryAs<SurfaceMesh>();
+    if(!geom)
+    {
+        return false;
+    }
+    return geom->isMeshTextured();
 }
 
 SIMMEDTK_BEGIN_DYNAMIC_LOADER()
-  SIMMEDTK_BEGIN_ONLOAD(register_mesh_render_delegate)
-    SIMMEDTK_REGISTER_CLASS(RenderDelegate,RenderDelegate,MeshRenderDelegate,2000);
-  SIMMEDTK_FINISH_ONLOAD()
+SIMMEDTK_BEGIN_ONLOAD(register_mesh_render_delegate)
+SIMMEDTK_REGISTER_CLASS(RenderDelegate,RenderDelegate,MeshRenderDelegate,2000);
+SIMMEDTK_FINISH_ONLOAD()
 SIMMEDTK_FINISH_DYNAMIC_LOADER()
