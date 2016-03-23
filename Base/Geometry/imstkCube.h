cfbf57fb39c1a281d4744e726a640244d242a774
Alexis Girault <alexis.girault@kitware.com>
2016-03-23 12:40:31 -0400

    ENH: Add imstkSphere and imstkCube

---
+++ Base/Geometry/imstkCube.h
@@ -0,0 +1,51 @@
+/*=========================================================================
+
+   Library: iMSTK
+
+   Copyright (c) Kitware, Inc. & Center for Modeling, Simulation,
+   & Imaging in Medicine, Rensselaer Polytechnic Institute.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0.txt
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   =========================================================================*/
+
+#ifndef imstkCube_h
+#define imstkCube_h
+
+#include "imstkAnalyticalGeometry.h"
+
+namespace imstk {
+class Cube : public AnalyticalGeometry
+{
+public:
+
+    Cube(const Vec3d & position = ORIGIN,
+         const double& width = 10) :
+        AnalyticalGeometry(GeometryType::Cube,
+                           position,
+                           Quatd()),
+        m_width(width)
+    {}
+
+    ~Cube() = default;
+
+    const double& getWidth() const;
+    void          setWidth(const double& width);
+
+protected:
+
+    double m_width;
+};
+}
+
+#endif // ifndef imstkCube_h
---
+++ Base/Geometry/imstkSphere.h
@@ -0,0 +1,51 @@
+/*=========================================================================
+
+   Library: iMSTK
+
+   Copyright (c) Kitware, Inc. & Center for Modeling, Simulation,
+   & Imaging in Medicine, Rensselaer Polytechnic Institute.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0.txt
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   =========================================================================*/
+
+#ifndef imstkSphere_h
+#define imstkSphere_h
+
+#include "imstkAnalyticalGeometry.h"
+
+namespace imstk {
+class Sphere : public AnalyticalGeometry
+{
+public:
+
+    Sphere(const Vec3d & position = ORIGIN,
+           const double& radius = 10) :
+        AnalyticalGeometry(GeometryType::Sphere,
+                           position,
+                           Quatd()),
+        m_radius(radius)
+    {}
+
+    ~Sphere() = default;
+
+    const double& getRadius() const;
+    void          setRadius(const double& radius);
+
+protected:
+
+    double m_radius;
+};
+}
+
+#endif // ifndef imstkSphere_h
