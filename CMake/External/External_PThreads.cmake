9c35a64c6ed014c2f8184d2dd228c39f91b2129e
Sreekanth Arikatla <sreekanth.arikatla@kitware.com>
2016-03-31 14:07:02 -0400

    WIP: Adding pthreads from Alexis

---
+++ CMake/External/External_PThreads.cmake
@@ -0,0 +1,10 @@
+#-----------------------------------------------------------------------------
+# Add External Project
+#-----------------------------------------------------------------------------
+include(imstkAddExternalProject)
+imstk_add_external_project( PThreads
+  REPOSITORY ${git_protocol}://github.com/songdongsheng/libpthread.git
+  GIT_TAG 38821d529407dcb0b4661c6805d7b1bf83f3204d
+  DEPENDENCIES ""
+  #VERBOSE
+  )
--- CMake/External/External_VegaFEM.cmake
+++ CMake/External/External_VegaFEM.cmake
@@ -1,6 +1,11 @@
 #-----------------------------------------------------------------------------
 # Add External Project
 #-----------------------------------------------------------------------------
+set(VegaFEM_DEPENDENCIES "")
+if(WIN32)
+  list(APPEND VegaFEM_DEPENDENCIES PThreads)
+endif(WIN32)
+
 include(imstkAddExternalProject)
 imstk_add_external_project( VegaFEM
   REPOSITORY git@gitlab.kitware.com:iMSTK/VegaFEM-CMake.git
@@ -10,6 +15,6 @@ imstk_add_external_project( VegaFEM
     -DVegaFEM_ENABLE_OpenGL_SUPPORT:BOOL=OFF
     -DVegaFEM_BUILD_MODEL_REDUCTION:BOOL=OFF
     -DVegaFEM_BUILD_UTILITIES:BOOL=ON
-  DEPENDENCIES ""
+  DEPENDENCIES ${VegaFEM_DEPENDENCIES}
   #VERBOSE
   )
