d04e7f3582c84eb9dada48d39f44be572eb96c4c
Alexis Girault <alexis.girault@kitware.com>
2016-04-01 12:42:00 -0400

    BUG: Update g3log version
    Corrects bug on Windows

--- CMake/External/External_g3log.cmake
+++ CMake/External/External_g3log.cmake
@@ -4,7 +4,7 @@
 include(imstkAddExternalProject)
 imstk_add_external_project( g3log
   REPOSITORY ${git_protocol}://github.com/KjellKod/g3log.git
-  GIT_TAG 579579962c94213a46fadbb70a996f08c19be3a2
+  GIT_TAG 6c1698c4f7db6b9e4246ead38051f9866ea3ac06
   RELATIVE_INCLUDE_PATH "/src"
   #DEPENDENCIES ""
   #VERBOSE
