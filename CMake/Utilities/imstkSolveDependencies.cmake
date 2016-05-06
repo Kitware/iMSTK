f510821b3a37fffb062aea7a7731d39b06dc71dd
Alexis Girault <alexis.girault@kitware.com>
2016-05-06 19:07:56 -0400

    STYLE: Update imstkSolveDependencies header

--- CMake/Utilities/imstkSolveDependencies.cmake
+++ CMake/Utilities/imstkSolveDependencies.cmake
@@ -1,41 +1,15 @@
-##############################################################################
-#
-# Library:   TubeTK
-#
-# Copyright 2010 Kitware Inc. 28 Corporate Drive,
-# Clifton Park, NY, 12065, USA.
-#
-# All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-##############################################################################
-
 #.rst:
 # ExternalProjectDependency
 # -------------------------
 
 ###########################################################################
 #
-#  Library:   CTK
+#  Program: 3D Slicer
 #
 #  Copyright (c) Kitware Inc.
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0.txt
+#  See COPYRIGHT.txt
+#  or http://www.slicer.org/copyright/copyright.txt for details.
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
@@ -612,7 +586,6 @@ macro(ExternalProject_Include_Dependencies project_name)
   # Include dependencies
   foreach(dep ${_sb_DEPENDS})
     get_property(_included GLOBAL PROPERTY SB_${dep}_FILE_INCLUDED)
-      #message("## SB_${dep}_included : ${_included}")
     if(NOT _included)
       # XXX - Refactor - Add a single variable named 'EXTERNAL_PROJECT_DIRS'
       if(EXISTS "${EXTERNAL_PROJECT_DIR}/${EXTERNAL_PROJECT_FILE_PREFIX}${dep}.cmake")
