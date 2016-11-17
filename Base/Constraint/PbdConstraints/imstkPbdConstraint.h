5487a363491a66eacd4e601dd5120930db8ab861
Sreekanth Arikatla <sreekanth.arikatla@kitware.com>
2016-11-17 14:53:16 -0500

    BUG: Fixes wrong default value for pbd tolerance
    Fixes wrong default value for pbd tolerance in PbdConstraint class

--- Base/Constraint/PbdConstraints/imstkPbdConstraint.h
+++ Base/Constraint/PbdConstraints/imstkPbdConstraint.h
@@ -74,7 +74,7 @@ public:
 
 public:
     std::vector<size_t> m_vertexIds; ///> index of points for the constraint
-    double m_epsilon = 1.0e6;         ///> Tolerance used for the costraints
+    double m_epsilon = 1.0e-6;         ///> Tolerance used for the costraints
 };
 }
 
