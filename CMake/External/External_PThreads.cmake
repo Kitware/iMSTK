8c5af99a00d85db33d12ff30bae8d428e4e8be36
Dženan Zukić <dzenan.zukic@kitware.com>
2017-03-22 09:53:46 -0400

    moving libpthread repo to iMSTK group

--- CMake/External/External_PThreads.cmake
+++ CMake/External/External_PThreads.cmake
@@ -3,7 +3,7 @@
 #-----------------------------------------------------------------------------
 include(imstkAddExternalProject)
 imstk_add_external_project( PThreads
-  GIT_REPOSITORY https://github.com/dzenanz/libpthread.git
+  GIT_REPOSITORY https://gitlab.kitware.com/iMSTK/libpthread.git
   GIT_TAG 40707ceea7c35b06f7448f61ae62422efe6a7873
   INSTALL_COMMAND ${SKIP_STEP_COMMAND}
   RELATIVE_INCLUDE_PATH "include"
