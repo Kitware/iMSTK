fcbb2ca0e6788814dd77f75892067529d16638dd
Sreekanth Arikatla <sreekanth.arikatla@kitware.com>
2017-09-11 15:45:32 -0400

    COMP: Replace the ssh links with https for some dependencies
    This will not mandate the users that checkout the code to set up ssh

--- CMake/External/External_VegaFEM.cmake
+++ CMake/External/External_VegaFEM.cmake
@@ -8,7 +8,7 @@ endif(WIN32)
 
 include(imstkAddExternalProject)
 imstk_add_external_project( VegaFEM
-  GIT_REPOSITORY git@gitlab.kitware.com:iMSTK/VegaFEM-CMake.git
+  GIT_REPOSITORY https://gitlab.kitware.com/iMSTK/VegaFEM-CMake.git
   GIT_TAG 7bc1bda6a873e2cf4486f9ec3fe6b7b90da76941
   INSTALL_COMMAND ${SKIP_STEP_COMMAND}
   CMAKE_ARGS
