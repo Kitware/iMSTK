ca4403da817ee45120bc1a476dfc3cff1a70238c
Nghia Truong <nghiatruong.vn@gmail.com>
2019-06-18 12:38:43 -0400

    ENH: Add parallel atomic operations

--- Source/Core/Parallel/imstkAtomicOperations.h
+++ Source/Core/Parallel/imstkAtomicOperations.h
@@ -21,3 +21,111 @@
 
 
 #pragma once
+#include <atomic>
+
+#include "imstkMath.h"
+
+namespace imstk
+{
+///
+/// \brief Perform an atomic operation: target = f(operand)
+///
+template<class T, class Function>
+void imstk_atomic_op(T& target, const T operand, Function&& f)
+{
+    std::atomic<T>& tgt = *(static_cast<std::atomic<T>*>(&target));
+
+    T cur_val = target;
+    T new_val;
+    do
+    {
+        new_val = f(cur_val, operand);
+    }
+    while(!tgt.compare_exchange_weak(cur_val, new_val));
+}
+
+///
+/// \brief Atomic addition for scalar numbers
+///
+template<class T>
+void imstk_atomic_add(T& target, const T operand)
+{
+    imstk_atomic_op(target, operand, [](T a, T b) { return a + b; });
+}
+
+///
+/// \brief Atomic subtraction for scalar numbers
+///
+template<class T>
+void imstk_atomic_subtract(T& target, const T operand)
+{
+    imstk_atomic_op(target, operand, [](T a, T b) { return a - b; });
+}
+
+
+///
+/// \brief Atomic multiplication for scalar numbers
+///
+template<class T>
+void imstk_atomic_multiply(T& target, const T operand)
+{
+    imstk_atomic_op(target, operand, [](T a, T b) { return a * b; });
+}
+
+///
+/// \brief Atomic division for scalar numbers
+///
+template<class T>
+void imstk_atomic_divide(T& target, T operand)
+{
+    imstk_atomic_op(target, operand, [](T a, T b) { return a / b; });
+}
+
+///
+/// \brief Atomic addition for two vectors
+///
+template<class T, int N>
+void imstk_atomic_add(Eigen::Matrix<T, N, 1>& target, const Eigen::Matrix<T, N, 1>& operand)
+{
+    for(int i = 0; i < N; ++i)
+    {
+        imstk_atomic_add(target[i], operand[i]);
+    }
+}
+
+///
+/// \brief Atomic subtraction for two vectors
+///
+template<class T, int N>
+void imstk_atomic_subtract(Eigen::Matrix<T, N, 1>& target, const Eigen::Matrix<T, N, 1>& operand)
+{
+    for(int i = 0; i < N; ++i)
+    {
+        imstk_atomic_subtract(target[i], operand[i]);
+    }
+}
+
+///
+/// \brief Atomic multiplication for two vectors
+///
+template<class T, int N>
+void imstk_atomic_multiply(Eigen::Matrix<T, N, 1>& target, const T operand)
+{
+    for(int i = 0; i < N; ++i)
+    {
+        imstk_atomic_multiply(target[i], operand);
+    }
+}
+
+///
+/// \brief Atomic division for two vectors
+///
+template<class T, int N>
+void imstk_atomic_divide(Eigen::Matrix<T, N, 1>& target, const T operand)
+{
+    for(int i = 0; i < N; ++i)
+    {
+        imstk_atomic_divide(target[i], operand);
+    }
+}
+}// end namespace imstk
