dcdfe5f1d96328858af62289994db70cc6beabf5
Aaron Bray <aaron.bray@kitware.com>
2019-12-02 08:06:13 -0500

    Patch VTK 8.2 to provide OpenVR support

---
+++ CMake/External/Patches/vtk-8.2.cmake
@@ -0,0 +1,6 @@
+
+
+file(COPY  ${VTK_SOURCE_DIR}/Common/Core/vtkEventData.h
+     DESTINATION ${CMAKE_INSTALL_PREFIX}/include/vtk-8.2/)
+file(COPY  ${VTK_SOURCE_DIR}/Rendering/OpenVR/vtkOpenVROverlayInternal.h
+     DESTINATION ${CMAKE_INSTALL_PREFIX}/include/vtk-8.2/)
