c501cf2471fdeecd1c8bd7cc54b22c79b7fb3444
Aaron Bray <aaron.bray@kitware.com>
2019-12-03 12:58:12 -0500

    Copy g3log so on linux

--- CMake/External/External_g3log.cmake
+++ CMake/External/External_g3log.cmake
@@ -2,14 +2,23 @@
 # Add External Project
 #-----------------------------------------------------------------------------
 
-if(MSVC)
-  set(g3log_built_library ${g3log_BINARY_DIR}/$(Configuration)/g3logger$<$<CONFIG:Debug>:d>.lib)
-else()
-  set(g3log_built_library ${g3log_BINARY_DIR}/libg3logger.a)
-endif()
-
 include(imstkAddExternalProject)
 imstk_define_external_dirs( g3log )
+
+if(MSVC)
+  set(g3log_built_library ${CMAKE_COMMAND} -E copy 
+                          ${g3log_BINARY_DIR}/$(Configuration)/g3logger$<$<CONFIG:Debug>:d>.lib
+                          ${CMAKE_INSTALL_PREFIX}/lib/)
+  set(g3log_built_shared)
+else()
+  set(g3log_built_library ${CMAKE_COMMAND} -E copy 
+                          ${g3log_BINARY_DIR}/libg3logger.a
+                          ${CMAKE_INSTALL_PREFIX}/lib/)
+  set(g3log_built_shared ${CMAKE_COMMAND} -E copy 
+                         ${g3log_BINARY_DIR}/libg3logger.so
+                         ${CMAKE_INSTALL_PREFIX}/lib/)
+endif()
+
 imstk_add_external_project( g3log
   GIT_REPOSITORY https://gitlab.kitware.com/iMSTK/g3log.git
   GIT_TAG 6c1698c4f7db6b9e4246ead38051f9866ea3ac06
@@ -17,7 +26,8 @@ imstk_add_external_project( g3log
     -DADD_FATAL_EXAMPLE:BOOL=OFF
   INSTALL_COMMAND
     COMMAND ${CMAKE_COMMAND} -E copy_directory ${g3log_SOURCE_DIR}/src/g3log ${CMAKE_INSTALL_PREFIX}/include/g3log
-    COMMAND ${CMAKE_COMMAND} -E copy ${g3log_built_library} ${CMAKE_INSTALL_PREFIX}/lib/
+    COMMAND ${g3log_built_library}
+    COMMAND ${g3log_built_shared}
   RELATIVE_INCLUDE_PATH "src"
   DEPENDENCIES ""
   #VERBOSE
