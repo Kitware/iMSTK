ff7f643cabb771f53b5adb95e54a72813df9d3d2
Aaron Bray <aaron.bray@kitware.com>
2020-03-04 10:40:04 -0500

    Change to  build PhysX Dynamic Libraries

--- CMake/FindPhysX.cmake
+++ CMake/FindPhysX.cmake
@@ -10,14 +10,15 @@ endif()
 
 imstk_find_header(PhysX PxPhysicsAPI.h physx)
 # NOTE: Link order matters on linux!
-imstk_find_libary(PhysX PhysX _static${physx_release_type}_64 _staticDEBUG_64)#Different release and debug postfixes
 imstk_find_libary(PhysX PhysXExtensions _static${physx_release_type}_64 _staticDEBUG_64)#Different release and debug postfixes
-imstk_find_libary(PhysX PhysXCooking _static${physx_release_type}_64 _staticDEBUG_64)#Different release and debug postfixes
-imstk_find_libary(PhysX PhysXCommon _static${physx_release_type}_64 _staticDEBUG_64)#Different release and debug postfixes
 imstk_find_libary(PhysX PhysXPvdSDK _static${physx_release_type}_64 _staticDEBUG_64)#Different release and debug postfixes
-imstk_find_libary(PhysX PhysXFoundation _static${physx_release_type}_64 _staticDEBUG_64)#Different release and debug postfixes
-imstk_find_libary(PhysX PhysXCharacterKinematic _static${physx_release_type}_64 _staticDEBUG_64)#Different release and debug postfixes
-imstk_find_libary(PhysX PhysXVehicle _static${physx_release_type}_64 _staticDEBUG_64)#Different release and debug postfixes
+#imstk_find_libary(PhysX PhysXCharacterKinematic _static${physx_release_type}_64 _staticDEBUG_64)#Different release and debug postfixes
+#imstk_find_libary(PhysX PhysXVehicle _static${physx_release_type}_64 _staticDEBUG_64)#Different release and debug postfixes
+
+imstk_find_libary(PhysX PhysX ${physx_release_type}_64 DEBUG_64)#Different release and debug postfixes
+imstk_find_libary(PhysX PhysXCooking ${physx_release_type}_64 DEBUG_64)#Different release and debug postfixes
+imstk_find_libary(PhysX PhysXCommon _${physx_release_type}_64 DEBUG_64)#Different release and debug postfixes
+imstk_find_libary(PhysX PhysXFoundation ${physx_release_type}_64 DEBUG_64)#Different release and debug postfixes
 
 imstk_find_package(PhysX)
 
