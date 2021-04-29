2e2c61cc3f77fcb7a7c4dd378ca4c49af416d024
Harald Scheirich <harald.scheirich@kitware.com>
2021-04-29 08:45:07 -0400

    Warn instead of CHECK for invalid number of components

--- Source/Geometry/Mesh/imstkPointSet.cpp
+++ Source/Geometry/Mesh/imstkPointSet.cpp
@@ -183,7 +183,8 @@ PointSet::enforceType(
     if (attributes[name]->getScalarType() != type)
     {
         auto attribute = attributes[name];
-        CHECK(attribute->getNumberOfComponents() == components) << label << "need to have " << components << "components in " << getName();
+        LOG_IF(WARNING, attribute->getNumberOfComponents() != components) << label << " need to have " << components 
+            << "components in " << getName() << " actual number : " << attribute->getNumberOfComponents();
         LOG(INFO) << "Geometry: " << getName() << " selected array for " << " not of type " << ", casting.";
         auto newArray = attribute->cast(type);
         attributes[name] = std::move(newArray);
