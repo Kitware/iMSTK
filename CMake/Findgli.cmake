c8943130d74f60d2755f28e27f91d850c5f5c7b9
Andrew Wilson <andx_roo@live.com>
2021-06-01 13:52:33 -0400

    REFAC: Vulkan removed

--- CMake/Findgli.cmake
+++
@@ -1,9 +0,0 @@
-include(imstkFind)
-#-----------------------------------------------------------------------------
-# Find All Headers and Libraries for glm
-#-----------------------------------------------------------------------------
-
-imstk_find_header(gli gli/gli.hpp)
-imstk_find_header_package(gli)
-
-#message(STATUS "gli include : ${GLI_INCLUDE_DIRS}")
