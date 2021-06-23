b425d9ca5122668c608a0a208373546f8e795213
sreekanth-arikatla <sreekanth.arikatla@kitware.com>
2021-06-23 17:34:26 -0400

    REFAC: Remove redundant public api in scene class
    Removes isLightRegistered, hasSceneObject
    Adds removeLight

--- Source/Rendering/VTKRenderer/imstkVTKRenderer.cpp
+++ Source/Rendering/VTKRenderer/imstkVTKRenderer.cpp
@@ -538,7 +538,8 @@ VTKRenderer::sceneModifed(Event* imstkNotUsed(e))
     // If the SceneObject is being rendered but not in the scene
     for (auto i = m_renderedObjects.begin(); i != m_renderedObjects.end(); i++)
     {
-        if (!m_scene->hasSceneObject(*i))
+        auto sos = m_scene->getSceneObjects();
+        if (sos.find(*i) == sos.end())
         {
             i = removeSceneObject(*i);
         }
