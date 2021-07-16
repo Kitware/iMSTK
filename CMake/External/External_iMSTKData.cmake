3c2ae8a6c9f34ee922d026dfd9724a5548a2915f
Harald Scheirich <harald.scheirich@kitware.com>
2021-07-16 11:19:12 -0400

    BUG: Fix issue with updating the hash

--- CMake/External/External_iMSTKData.cmake
+++ CMake/External/External_iMSTKData.cmake
@@ -9,11 +9,19 @@ set(copy_data_command
   ${CMAKE_INSTALL_PREFIX}/data
   )
 
+# HS - Due to an issue where it seems that the repository does not get updated
+# we need to call fetch here to refresh the index, this way the checkout can 
+# succeed for the add_external_project call
+execute_process(
+  COMMAND ${GIT_COMMAND} fetch --
+  WORKING_DIRECTORY ${iMSTKData_PREFIX}/src
+  RESULT_VARIABLE error_code
+  )
 
 include(imstkAddExternalProject)
 imstk_add_external_project( iMSTKData
   GIT_REPOSITORY  "https://gitlab.kitware.com/iMSTK/imstk-data.git"
-  GIT_TAG  "69d0d66ed040061abf176af41c3c3d1742203cbb"
+  GIT_TAG  "cd4cd59bcf86e4a9fed47e145e482990390e3c09"
   CONFIGURE_COMMAND ${SKIP_STEP_COMMAND}
   BUILD_COMMAND ${SKIP_STEP_COMMAND}
   INSTALL_COMMAND COMMAND ${copy_data_command}
