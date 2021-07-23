b5fc822152f56ec6585cb580c2c8687463adf852
Harald Scheirich <harald.scheirich@kitware.com>
2021-07-23 13:48:46 -0400

    ENH: find the git-lfs executable

--- CMake/External/External_iMSTKData.cmake
+++ CMake/External/External_iMSTKData.cmake
@@ -3,6 +3,12 @@
 #-----------------------------------------------------------------------------
 set(iMSTKData_PREFIX ${CMAKE_BINARY_DIR}/External/iMSTKData)
 
+find_package(GitLFS QUIET)
+if (NOT GitLFS_FOUND)
+  message(FATAL_ERROR " Building the tests or the examples requires the iMSTK data, which in turn\
+  requires git-lfs, make sure it is installed on your system, on linux please run `sudo apt-get install git-lfs`")
+endif()
+
 set(copy_data_command
   ${CMAKE_COMMAND} -E copy_directory
   ${iMSTKData_PREFIX}/src/Data
