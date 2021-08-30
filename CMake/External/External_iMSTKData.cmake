2ae1ca61f0dd58b3b1c7625dbad8f4b82923f90c
Harald Scheirich <harald.scheirich@kitware.com>
2021-08-30 13:43:33 -0400

    BUG: Fix trailing slash
    fix version download
    update data sha
    add target

--- CMake/External/External_iMSTKData.cmake
+++ CMake/External/External_iMSTKData.cmake
@@ -2,6 +2,7 @@
 # Add External Project
 #-----------------------------------------------------------------------------
 set(iMSTKData_PREFIX ${CMAKE_BINARY_DIR}/External/iMSTKData)
+set(iMSTKData_SOURCE ${iMSTKData_PREFIX}/src/Data CACHE INTERNAL "")
 
 find_package(GitLFS QUIET)
 if (NOT GitLFS_FOUND)
@@ -19,7 +20,7 @@ set(copy_data_command
 # we need to call fetch here to refresh the index, this way the checkout can 
 # succeed for the add_external_project call
 execute_process(
-  COMMAND ${GIT_COMMAND} fetch --
+  COMMAND ${GIT_EXECUTABLE} fetch --all
   WORKING_DIRECTORY ${iMSTKData_PREFIX}/src
   RESULT_VARIABLE error_code
   )
@@ -27,7 +28,7 @@ execute_process(
 include(imstkAddExternalProject)
 imstk_add_external_project( iMSTKData
   GIT_REPOSITORY  "https://gitlab.kitware.com/iMSTK/imstk-data.git"
-  GIT_TAG  "cd4cd59bcf86e4a9fed47e145e482990390e3c09"
+  GIT_TAG  "14824e3d53328ed6be481981959780f32881030b"
   CONFIGURE_COMMAND ${SKIP_STEP_COMMAND}
   BUILD_COMMAND ${SKIP_STEP_COMMAND}
   INSTALL_COMMAND COMMAND ${copy_data_command}
