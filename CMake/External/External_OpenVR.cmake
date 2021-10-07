99bea6a5e7d841b90beb762e0498abad918379cf
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-10-07 18:10:33 -0400

    COMP: Simplify OpenVR external project removing OPENVR_EXTRACT_DIR ivar

--- CMake/External/External_OpenVR.cmake
+++ CMake/External/External_OpenVR.cmake
@@ -6,44 +6,44 @@ imstk_define_external_dirs( OpenVR )
 #-----------------------------------------------------------------------------
 # Set install commands
 #-----------------------------------------------------------------------------
-set(OPENVR_EXTRACT_DIR ${OpenVR_SOURCE_DIR})
+set(OpenVR_INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
 
 if(WIN32)
-  set(openvr_libdir "${OPENVR_EXTRACT_DIR}/lib/win64")
+  set(openvr_libdir "${OpenVR_SOURCE_DIR}/lib/win64")
   set(openvr_libname "openvr_api.lib")
 elseif(APPLE)
-  set(openvr_libdir "${OPENVR_EXTRACT_DIR}/lib/osx32")
+  set(openvr_libdir "${OpenVR_SOURCE_DIR}/lib/osx32")
   set(openvr_libname "libopenvr_api.dylib")
 elseif(UNIX)
-  set(openvr_libdir "${OPENVR_EXTRACT_DIR}/lib/linux64")
+  set(openvr_libdir "${OpenVR_SOURCE_DIR}/lib/linux64")
   set(openvr_libname "libopenvr_api.so")
 endif()
 
 if(WIN32)
-  set(openvr_dlldir "${OPENVR_EXTRACT_DIR}/bin/win64")
+  set(openvr_dlldir "${OpenVR_SOURCE_DIR}/bin/win64")
   set(openvr_dllname "openvr_api.dll")
 elseif(APPLE)
-  set(openvr_dlldir "${OPENVR_EXTRACT_DIR}/bin/osx64")
+  set(openvr_dlldir "${OpenVR_SOURCE_DIR}/bin/osx64")
   set(openvr_dllname "OpenVR.framework")
 elseif(UNIX)
-  set(openvr_dlldir "${OPENVR_EXTRACT_DIR}/bin/linux64")
+  set(openvr_dlldir "${OpenVR_SOURCE_DIR}/bin/linux64")
   set(openvr_dllname "libopenvr_api.so")
 endif()
 
 set(copy_openvr_headers_command
   ${CMAKE_COMMAND} -E copy_directory
-  ${OPENVR_EXTRACT_DIR}/headers
-  ${CMAKE_INSTALL_PREFIX}/include/OpenVR
+  ${OpenVR_SOURCE_DIR}/headers
+  ${OpenVR_INSTALL_DIR}/include/OpenVR
   )
 set(copy_openvr_dll_command
   ${CMAKE_COMMAND} -E copy
   ${openvr_dlldir}/${openvr_dllname}
-  ${CMAKE_INSTALL_PREFIX}/bin/${openvr_dllname}
+  ${OpenVR_INSTALL_DIR}/bin/${openvr_dllname}
   )
 set(copy_openvr_lib_command
   ${CMAKE_COMMAND} -E copy
   ${openvr_libdir}/${openvr_libname}
-  ${CMAKE_INSTALL_PREFIX}/lib/${openvr_libname}
+  ${OpenVR_INSTALL_DIR}/lib/${openvr_libname}
   )
 
 #-----------------------------------------------------------------------------
