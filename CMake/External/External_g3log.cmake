912d37aba42393e6b42a98aee732db4b9ecb78a0
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-10-07 04:22:32 -0400

    COMP: Update g3log external project to support reuse from application
    This commit updates the project to specify the install commands
    responsible to copy file to CMAKE_INSTALL_PREFIX only if *no*
    custom install directory was specified.
    
    It updates the logic to use g3log_INSTALL_DIR instead of CMAKE_INSTALL_PREFIX.
    
    It ensures the install subdirectory exists when custom location is
    specified.

--- CMake/External/External_g3log.cmake
+++ CMake/External/External_g3log.cmake
@@ -3,31 +3,47 @@
 #-----------------------------------------------------------------------------
 
 include(imstkAddExternalProject)
+
+# Set g3log_SOURCE_DIR and g3log_BINARY_DIR
 imstk_define_external_dirs( g3log )
 
-if(MSVC)
-  set(g3log_built_library ${CMAKE_COMMAND} -E copy 
-                          ${g3log_BINARY_DIR}/$(Configuration)/g3logger$<$<CONFIG:Debug>:d>.lib
-                          ${CMAKE_INSTALL_PREFIX}/lib/)
-  set(g3log_built_shared)
-else()
-  set(g3log_built_library ${CMAKE_COMMAND} -E copy 
-                          ${g3log_BINARY_DIR}/libg3logger.a
-                          ${CMAKE_INSTALL_PREFIX}/lib/)
-  set(g3log_built_shared ${CMAKE_COMMAND} -E copy 
-                         ${g3log_BINARY_DIR}/libg3logger.so
-                         ${CMAKE_INSTALL_PREFIX}/lib/)
+#-----------------------------------------------------------------------------
+# Set install commands
+#-----------------------------------------------------------------------------
+if(CMAKE_PROJECT_NAME STREQUAL "iMSTK")
+  set(g3log_INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
 endif()
 
+if(MSVC)
+  set(g3log_built_library ${CMAKE_COMMAND} -E copy
+                          ${g3log_BINARY_DIR}/$(Configuration)/g3logger$<$<CONFIG:Debug>:d>.lib
+                          ${g3log_INSTALL_DIR}/lib/g3logger$<$<CONFIG:Debug>:d>.lib)
+  set(g3log_built_shared)
+else()
+  set(g3log_built_library ${CMAKE_COMMAND} -E copy
+                          ${g3log_BINARY_DIR}/libg3logger.a
+                          ${g3log_INSTALL_DIR}/lib/libg3logger.a)
+  set(g3log_built_shared ${CMAKE_COMMAND} -E copy
+                         ${g3log_BINARY_DIR}/libg3logger.so
+                         ${g3log_INSTALL_DIR}/lib/libg3logger.so)
+endif()
+
+set(g3log_INSTALL_COMMAND
+  INSTALL_COMMAND
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${g3log_SOURCE_DIR}/src/g3log ${g3log_INSTALL_DIR}/include/g3log
+    COMMAND ${g3log_built_library}
+    COMMAND ${g3log_built_shared}
+  )
+
+#-----------------------------------------------------------------------------
+# Add External Project
+#-----------------------------------------------------------------------------
 imstk_add_external_project( g3log
   URL https://gitlab.kitware.com/iMSTK/g3log/-/archive/6c1698c4f7db6b9e4246ead38051f9866ea3ac06/archive.zip
   URL_MD5 3815bbfec2ff51dc473063c35eec0f36
   CMAKE_CACHE_ARGS
     -DADD_FATAL_EXAMPLE:BOOL=OFF
-  INSTALL_COMMAND
-    COMMAND ${CMAKE_COMMAND} -E copy_directory ${g3log_SOURCE_DIR}/src/g3log ${CMAKE_INSTALL_PREFIX}/include/g3log
-    COMMAND ${g3log_built_library}
-    COMMAND ${g3log_built_shared}
+  ${g3log_INSTALL_COMMAND}
   RELATIVE_INCLUDE_PATH "src"
   DEPENDENCIES ""
   #VERBOSE
