7c16ecd8a1328d15897b1d43e1d61a79b649c5a7
Andrew Wilson <andx_roo@live.com>
2021-11-12 01:24:06 -0500

    STYLE: Uncrustify a second time

--- Source/Controllers/Testing/imstkTrackingDeviceControlTest.cpp
+++ Source/Controllers/Testing/imstkTrackingDeviceControlTest.cpp
@@ -64,63 +64,67 @@ public:
 
 TEST_F(TrackingDeviceControlTest, Basics)
 {
-        control.setDevice(nullptr);
-        EXPECT_FALSE(control.updateTrackingData(0.0));
+    control.setDevice(nullptr);
+    EXPECT_FALSE(control.updateTrackingData(0.0));
 
-        control.setDevice(client);
-        EXPECT_TRUE(control.updateTrackingData(0.0));
+    control.setDevice(client);
+    EXPECT_TRUE(control.updateTrackingData(0.0));
 
-        auto pos = Vec3d(1.0, 2.0, 3.0);
-        auto rot = Quatd(4.0, 5.0, 6.0, 7.0).normalized();
+    auto pos = Vec3d(1.0, 2.0, 3.0);
+    auto rot = Quatd(4.0, 5.0, 6.0, 7.0).normalized();
 
-        client->setPosition(pos);
-        client->setOrientation(rot);
+    client->setPosition(pos);
+    client->setOrientation(rot);
 
-        control.updateTrackingData(0.0);
+    control.updateTrackingData(0.0);
 
-        EXPECT_TRUE(pos.isApprox(control.getPosition()));
-        EXPECT_TRUE(rot.isApprox(control.getOrientation()));
+    EXPECT_TRUE(pos.isApprox(control.getPosition()));
+    EXPECT_TRUE(rot.isApprox(control.getOrientation()));
 }
 
 TEST_F(TrackingDeviceControlTest, EndEffector)
 {
-        auto offset = Quatd(Eigen::AngleAxisd(PI * 0.5, Eigen::Vector3d::UnitY()));
+    auto offset = Quatd(Eigen::AngleAxisd(PI * 0.5, Eigen::Vector3d::UnitY()));
 
-        control.setEffectorRotationOffset(offset);
+    control.setEffectorRotationOffset(offset);
 
-        auto pos = Vec3d(1.0, 2.0, 3.0);
-        auto rot = Quatd(4.0, 5.0, 6.0, 7.0).normalized();
+    auto pos = Vec3d(1.0, 2.0, 3.0);
+    auto rot = Quatd(4.0, 5.0, 6.0, 7.0).normalized();
 
-        client->setPosition(pos);
-        client->setOrientation(rot);
+    client->setPosition(pos);
+    client->setOrientation(rot);
 
-        control.updateTrackingData(0.0);
+    control.updateTrackingData(0.0);
 
-        auto expected = offset * rot;
+    auto expected = offset * rot;
 
-        EXPECT_TRUE(pos.isApprox(control.getPosition())) << "Expected: " << pos.transpose() << " Actual: " << control.getPosition();
-        EXPECT_TRUE(expected.isApprox(control.getOrientation())) << "Expected: " << expected.coeffs().transpose() << " Actual: " << control.getOrientation().coeffs().transpose();
+    EXPECT_TRUE(pos.isApprox(control.getPosition()))
+        << "Expected: " << pos.transpose()
+        << " Actual: " << control.getPosition();
+    EXPECT_TRUE(expected.isApprox(control.getOrientation()))
+        << "Expected: " << expected.coeffs().transpose()
+        << " Actual: " << control.getOrientation().coeffs().transpose();
 }
 
 TEST_F(TrackingDeviceControlTest, InvertTranslation)
 {
-        control.setInversionFlags(
-                TrackingDeviceControl::InvertFlag::transX |
-                TrackingDeviceControl::InvertFlag::transY |
-                TrackingDeviceControl::InvertFlag::transZ);
+    control.setInversionFlags(
+            TrackingDeviceControl::InvertFlag::transX |
+            TrackingDeviceControl::InvertFlag::transY |
+            TrackingDeviceControl::InvertFlag::transZ);
 
-        auto pos = Vec3d(1.0, 2.0, 3.0);
-        auto rot = Quatd(4.0, 5.0, 6.0, 7.0).normalized();
+    auto pos = Vec3d(1.0, 2.0, 3.0);
+    auto rot = Quatd(4.0, 5.0, 6.0, 7.0).normalized();
 
-        client->setPosition(pos);
-        client->setOrientation(rot);
+    client->setPosition(pos);
+    client->setOrientation(rot);
 
-        control.updateTrackingData(0.0);
+    control.updateTrackingData(0.0);
 
-        EXPECT_TRUE((pos * -1.0).isApprox(control.getPosition()))
-                << "Expected: " << (pos * -1).transpose() << " Actual: " << control.getPosition().transpose();
-        EXPECT_TRUE(rot.isApprox(control.getOrientation()))
-                << "Expected: " << rot.coeffs().transpose() << " Actual: " << control.getOrientation().coeffs().transpose();
+    EXPECT_TRUE((pos * -1.0).isApprox(control.getPosition()))
+            << "Expected: " << (pos * -1).transpose() << " Actual: " << control.getPosition().transpose();
+    EXPECT_TRUE(rot.isApprox(control.getOrientation()))
+            << "Expected: " << rot.coeffs().transpose() << " Actual: " << control.getOrientation().coeffs().transpose();
 }
 
 TEST_F(TrackingDeviceControlTest, InvertOrientation)
@@ -131,8 +135,8 @@ TEST_F(TrackingDeviceControlTest, InvertOrientation)
     // | /
     // +-----> +x
 
-        Vec3d                             dir[] = { Vec3d(1.0, 0.0, 0.0), Vec3d(0.0, 1.0, 0.0), Vec3d(0.0, 0.0, 1.0) };
-        TrackingDeviceControl::InvertFlag invertFlags[] =
+    Vec3d                             dir[] = { Vec3d(1.0, 0.0, 0.0), Vec3d(0.0, 1.0, 0.0), Vec3d(0.0, 0.0, 1.0) };
+    TrackingDeviceControl::InvertFlag invertFlags[] =
     {
         TrackingDeviceControl::InvertFlag::rotX,
         TrackingDeviceControl::InvertFlag::rotY,
@@ -140,7 +144,7 @@ TEST_F(TrackingDeviceControlTest, InvertOrientation)
         };
 
     // Inverts for rotations happen on a plane so invert X will flip Y & Z rotations.
-        for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         // 45 deg rotation around one of the axes that is not this one (i+1)%3
         Quatd orientation = Quatd(Rotd(PI_2 * 0.5, dir[(i + 1) % 3]));
@@ -165,26 +169,28 @@ TEST_F(TrackingDeviceControlTest, InvertOrientation)
             << "Direction: " << dir[i].transpose() << std::endl
             << "Expected: " << expectedOrientation << std::endl
             << "Actual: " << actualOrientation;
-        }
+    }
 }
 
 TEST_F(TrackingDeviceControlTest, TranslationOffset)
 {
-        auto offset = Vec3d(4.0, 5.0, 6.0);
-        control.setTranslationOffset(offset);
+    auto offset = Vec3d(4.0, 5.0, 6.0);
+    control.setTranslationOffset(offset);
 
-        auto pos = Vec3d(1.0, 2.0, 3.0);
-        auto rot = Quatd(4.0, 5.0, 6.0, 7.0).normalized();
+    auto pos = Vec3d(1.0, 2.0, 3.0);
+    auto rot = Quatd(4.0, 5.0, 6.0, 7.0).normalized();
 
-        client->setPosition(pos);
-        client->setOrientation(rot);
+    client->setPosition(pos);
+    client->setOrientation(rot);
 
-        control.updateTrackingData(0.0);
+    control.updateTrackingData(0.0);
 
-        Vec3d expectedPos = pos + offset;
-        Quatd expectedRot = rot;
-        EXPECT_TRUE(expectedPos.isApprox(control.getPosition()))
-                << "Expected: " << expectedPos.transpose() << " Actual: " << control.getPosition();
-        EXPECT_TRUE(expectedRot.isApprox(control.getOrientation()))
-                << "Expected: " << expectedRot.coeffs().transpose() << " Actual: " << control.getOrientation().coeffs().transpose();
+    Vec3d expectedPos = pos + offset;
+    Quatd expectedRot = rot;
+    EXPECT_TRUE(expectedPos.isApprox(control.getPosition()))
+        << "Expected: " << expectedPos.transpose()
+        << " Actual: " << control.getPosition();
+    EXPECT_TRUE(expectedRot.isApprox(control.getOrientation()))
+        << "Expected: " << expectedRot.coeffs().transpose()
+        << " Actual: " << control.getOrientation().coeffs().transpose();
 }
