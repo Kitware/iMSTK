01df91bcf1b1fe1951c9052b5ee93627829fd69f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-11-12 18:35:15 -0500

    COMP: Simplify FTD2XX external project removing FTD2XX_EXTRACT_DIR ivar

--- CMake/External/External_FTD2XX.cmake
+++ CMake/External/External_FTD2XX.cmake
@@ -12,7 +12,7 @@ imstk_define_external_dirs( FTD2XX )
 #-----------------------------------------------------------------------------
 # Set install commands
 #-----------------------------------------------------------------------------
-set(FTD2XX_EXTRACT_DIR ${FTD2XX_SOURCE_DIR})
+set(FTD2XX_INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
 
 set(ftd2xx_libdir "i386")
 if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
@@ -21,19 +21,19 @@ endif()
 
 set(copy_ftd2xx_headers_command
   ${CMAKE_COMMAND} -E copy
-  ${FTD2XX_EXTRACT_DIR}/ftd2xx.h
-  ${CMAKE_INSTALL_PREFIX}/include/ftd2xx/ftd2xx.h
+  ${FTD2XX_SOURCE_DIR}/ftd2xx.h
+  ${FTD2XX_INSTALL_DIR}/include/ftd2xx/ftd2xx.h
   )
 set(copy_ftd2xx_lib_command
   ${CMAKE_COMMAND} -E copy
-  ${FTD2XX_EXTRACT_DIR}/${ftd2xx_libdir}/ftd2xx.lib
-  ${CMAKE_INSTALL_PREFIX}/lib/ftd2xx.lib
+  ${FTD2XX_SOURCE_DIR}/${ftd2xx_libdir}/ftd2xx.lib
+  ${FTD2XX_INSTALL_DIR}/lib/ftd2xx.lib
   )
   
 set(copy_ftd2xx_dll_command
   ${CMAKE_COMMAND} -E copy
-  ${FTD2XX_EXTRACT_DIR}/${ftd2xx_libdir}/ftd2xx64.dll
-  ${CMAKE_INSTALL_PREFIX}/bin/ftd2xx64.dll
+  ${FTD2XX_SOURCE_DIR}/${ftd2xx_libdir}/ftd2xx64.dll
+  ${FTD2XX_INSTALL_DIR}/bin/ftd2xx64.dll
   )
 
 # HS 2021-aug-08 Some dependencies expect the dll name to 
@@ -41,8 +41,8 @@ set(copy_ftd2xx_dll_command
 # to the renamed version as the whole chain is built via 64 bit builds
 set(copy_ftd2xx_dll_command_rename
   ${CMAKE_COMMAND} -E copy
-  ${FTD2XX_EXTRACT_DIR}/${ftd2xx_libdir}/ftd2xx64.dll
-  ${CMAKE_INSTALL_PREFIX}/bin/ftd2xx.dll
+  ${FTD2XX_SOURCE_DIR}/${ftd2xx_libdir}/ftd2xx64.dll
+  ${FTD2XX_INSTALL_DIR}/bin/ftd2xx.dll
   )
 
 
