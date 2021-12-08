c910f3aa0f040328fbc616d9ef9dc827983791b1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-12-08 14:57:49 -0500

    COMP: Support customization of install dir associated with VegaFEM and Eigen3

--- CMake/External/External_Eigen3.cmake
+++ CMake/External/External_Eigen3.cmake
@@ -1,7 +1,13 @@
+
+include(imstkAddExternalProject)
+
+if(CMAKE_PROJECT_NAME STREQUAL "iMSTK")
+  set(Eigen3_INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
+endif()
+
 #-----------------------------------------------------------------------------
 # Add External Project
 #-----------------------------------------------------------------------------
-include(imstkAddExternalProject)
 imstk_add_external_project( Eigen3
   URL "https://gitlab.kitware.com/iMSTK/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz"
   URL_HASH MD5=4c527a9171d71a72a9d4186e65bea559
@@ -11,7 +17,8 @@ imstk_add_external_project( Eigen3
   DEPENDENCIES ""
   #VERBOSE
 )
+
 if(NOT USE_SYSTEM_Eigen3)
-  set(Eigen3_DIR ${CMAKE_INSTALL_PREFIX}/share/eigen3/cmake)
+  set(Eigen3_DIR ${Eigen3_INSTALL_DIR}/share/eigen3/cmake)
   #message(STATUS "Eigen3_DIR : ${Eigen3_DIR}")
 endif()
--- CMake/External/External_VegaFEM.cmake
+++ CMake/External/External_VegaFEM.cmake
@@ -1,3 +1,10 @@
+
+include(imstkAddExternalProject)
+
+if(CMAKE_PROJECT_NAME STREQUAL "iMSTK")
+  set(VegaFEM_INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
+endif()
+
 #-----------------------------------------------------------------------------
 # Add External Project
 #-----------------------------------------------------------------------------
@@ -6,7 +13,6 @@ if(WIN32)
   list(APPEND VegaFEM_DEPENDENCIES PThreads)
 endif(WIN32)
 
-include(imstkAddExternalProject)
 imstk_add_external_project( VegaFEM
   URL https://gitlab.kitware.com/iMSTK/vegafemv4.0/-/archive/build_model_reduction/vegafemv4.0-build_model_reduction.zip
   URL_MD5 3f04bb7c2ba080785bcadf44d1a462a3
@@ -19,8 +25,9 @@ imstk_add_external_project( VegaFEM
   RELATIVE_INCLUDE_PATH ""
   #VERBOSE
 )
+
 if(NOT USE_SYSTEM_VegaFEM)
-  set(VegaFEM_DIR ${CMAKE_INSTALL_PREFIX}/lib/cmake/VegaFEM)
+  set(VegaFEM_DIR ${VegaFEM_INSTALL_DIR}/lib/cmake/VegaFEM)
   #message(STATUS "VegaFEM_DIR : ${VegaFEM_DIR}")
 endif()
 
