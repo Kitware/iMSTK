70ca9b552a8631cae2aeb4154800f2a08005106a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-12-08 01:01:00 -0500

    COMP: Avoid unexpected lookup of Eigen3 and VegaFM disabling package registry export
    After updating the CMake minimum required version to >= 3.15, package
    export is disabled by default (see policy CMP0090) and explicitly setting
    option CMAKE_EXPORT_NO_PACKAGE_REGISTRY will not be needed.
    
    References:
    * https://cmake.org/cmake/help/latest/variable/CMAKE_FIND_PACKAGE_NO_SYSTEM_PACKAGE_REGISTRY.html#variable:CMAKE_FIND_PACKAGE_NO_SYSTEM_PACKAGE_REGISTRY
    * https://cmake.org/cmake/help/latest/manual/cmake-packages.7.html#disabling-the-package-registry

--- CMake/External/External_Eigen3.cmake
+++ CMake/External/External_Eigen3.cmake
@@ -8,11 +8,26 @@ endif()
 #-----------------------------------------------------------------------------
 # Add External Project
 #-----------------------------------------------------------------------------
+set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
+# After updating the CMake minimum required version to >= 3.15, package
+# export is disabled by default (see policy CMP0090) and explicitly setting
+# these options will not be needed.
+if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.15")
+  list(APPEND EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS
+    -DCMAKE_EXPORT_PACKAGE_REGISTRY:BOOL=OFF
+    )
+else()
+  list(APPEND EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS
+    -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY:BOOL=ON
+    )
+endif()
+
 imstk_add_external_project( Eigen3
   URL "https://gitlab.kitware.com/iMSTK/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz"
   URL_HASH MD5=4c527a9171d71a72a9d4186e65bea559
   CMAKE_CACHE_ARGS
         -DBUILD_TESTING:BOOL=OFF
+        ${EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS}
   RELATIVE_INCLUDE_PATH ""
   DEPENDENCIES ""
   #VERBOSE
--- CMake/External/External_VegaFEM.cmake
+++ CMake/External/External_VegaFEM.cmake
@@ -13,6 +13,20 @@ if(WIN32)
   list(APPEND VegaFEM_DEPENDENCIES PThreads)
 endif(WIN32)
 
+set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
+# After updating the CMake minimum required version to >= 3.15, package
+# export is disabled by default (see policy CMP0090) and explicitly setting
+# these options will not be needed.
+if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.15")
+  list(APPEND EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS
+    -DCMAKE_EXPORT_PACKAGE_REGISTRY:BOOL=OFF
+    )
+else()
+  list(APPEND EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS
+    -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY:BOOL=ON
+    )
+endif()
+
 imstk_add_external_project( VegaFEM
   URL https://gitlab.kitware.com/iMSTK/vegafemv4.0/-/archive/build_model_reduction/vegafemv4.0-build_model_reduction.zip
   URL_MD5 3f04bb7c2ba080785bcadf44d1a462a3
@@ -21,6 +35,7 @@ imstk_add_external_project( VegaFEM
     -DVegaFEM_ENABLE_OpenGL_SUPPORT:BOOL=OFF
     -DVegaFEM_BUILD_MODEL_REDUCTION:BOOL=${${PROJECT_NAME}_USE_MODEL_REDUCTION}
     -DVegaFEM_BUILD_UTILITIES:BOOL=OFF
+    ${EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS}
   DEPENDENCIES ${VegaFEM_DEPENDENCIES}
   RELATIVE_INCLUDE_PATH ""
   #VERBOSE
