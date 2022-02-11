9b901a6d7bdfbdcdd84ba159cc78dd4f8e747445
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-02-11 02:32:30 -0500

    COMP: Improve source download strategy for Assimp project
    This commit partially reverts ce3d6a91d (Convert external projects to zips)
    
    It avoids check-sum verification failure in the following scenarios:
    
    - the branch with the patches is updated to include additional patches.
    
    - the git server re-generate the cached archives with a new approach
      leading to different checksums.
      see [1], [2] and [3] for discussion of similar issues with eiter GitLab
      or GitHub
    
    It also references the branch imstk-v3.3.1-2016-07-08-a8673d482 (equivalent
    to fixCompilationError). The associated naming convention is documented
    at https://gitlab.kitware.com/iMSTK/assimp/-/blob/welcome/README.md
    
    [1] Gitlab release tarball stability
        https://forum.gitlab.com/t/gitlab-release-tarball-stability/41888
    
    [2] Why are the md5 checksums for download archives different?
        https://forum.gitlab.com/t/why-are-the-md5-checksums-for-download-archives-different/41649/2
    
    [3] Github Archive URL checksums have changed
        https://github.com/spack/spack/issues/5411

--- CMake/External/External_Assimp.cmake
+++ CMake/External/External_Assimp.cmake
@@ -5,22 +5,15 @@ include(imstkAddExternalProject)
 
 # Download options
 if(NOT DEFINED iMSTK_Assimp_GIT_SHA)
-  set(iMSTK_Assimp_GIT_SHA "fixCompilationError")
+  set(iMSTK_Assimp_GIT_SHA "ce3d6a91d2e0e945493c0fdeb162b50c9034e5f9") # imstk-v3.3.1-2016-07-08-a8673d482
 endif()
 if(NOT DEFINED iMSTK_Assimp_GIT_REPOSITORY)
-  set(EXTERNAL_PROJECT_DOWNLOAD_OPTIONS
-    URL https://gitlab.kitware.com/iMSTK/assimp/-/archive/${iMSTK_Assimp_GIT_SHA}/assimp-${iMSTK_Assimp_GIT_SHA}.zip
-    URL_HASH MD5=fab081af9dbd73a9a5c61cd33dd93340
-    )
-else()
-  set(EXTERNAL_PROJECT_DOWNLOAD_OPTIONS
-    GIT_REPOSITORY ${iMSTK_Assimp_GIT_REPOSITORY}
-    GIT_TAG ${iMSTK_Assimp_GIT_SHA}
-    )
+  set(iMSTK_Assimp_GIT_REPOSITORY "https://gitlab.kitware.com/iMSTK/assimp.git")
 endif()
 
 imstk_add_external_project( Assimp
-  ${EXTERNAL_PROJECT_DOWNLOAD_OPTIONS}
+  GIT_REPOSITORY ${iMSTK_Assimp_GIT_REPOSITORY}
+  GIT_TAG ${iMSTK_Assimp_GIT_SHA}
   CMAKE_CACHE_ARGS
     -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
     -DASSIMP_BUILD_ASSIMP_TOOLS:BOOL=OFF
