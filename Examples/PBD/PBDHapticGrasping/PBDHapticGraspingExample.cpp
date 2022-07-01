8eeebcb4e981a11e56de5e5f0418a3e9303987f4
Andrew Wilson <andx_roo@live.com>
2022-07-01 19:09:18 -0400

    REFAC: Use toTetGrid function in haptic grasping example

--- Examples/PBD/PBDHapticGrasping/PBDHapticGraspingExample.cpp
+++ Examples/PBD/PBDHapticGrasping/PBDHapticGraspingExample.cpp
@@ -50,101 +50,6 @@
 
 using namespace imstk;
 
-///
-/// \brief Creates a tetraheral grid
-/// \param physical dimension of tissue
-/// \param dimensions of tetrahedral grid used for tissue
-/// \param center of grid
-///
-static std::shared_ptr<TetrahedralMesh>
-makeTetGrid(const Vec3d& size, const Vec3i& dim, const Vec3d& center)
-{
-    imstkNew<TetrahedralMesh> prismMesh;
-
-    imstkNew<VecDataArray<double, 3>> verticesPtr(dim[0] * dim[1] * dim[2]);
-    VecDataArray<double, 3>&          vertices = *verticesPtr.get();
-    const Vec3d                       dx       = size.cwiseQuotient((dim - Vec3i(1, 1, 1)).cast<double>());
-    for (int z = 0; z < dim[2]; z++)
-    {
-        for (int y = 0; y < dim[1]; y++)
-        {
-            for (int x = 0; x < dim[0]; x++)
-            {
-                vertices[x + dim[0] * (y + dim[1] * z)] = Vec3i(x, y, z).cast<double>().cwiseProduct(dx) - size * 0.5 + center;
-            }
-        }
-    }
-
-    // Add connectivity data
-    imstkNew<VecDataArray<int, 4>> indicesPtr;
-    VecDataArray<int, 4>&          indices = *indicesPtr.get();
-    for (int z = 0; z < dim[2] - 1; z++)
-    {
-        for (int y = 0; y < dim[1] - 1; y++)
-        {
-            for (int x = 0; x < dim[0] - 1; x++)
-            {
-                int cubeIndices[8] =
-                {
-                    x + dim[0] * (y + dim[1] * z),
-                    (x + 1) + dim[0] * (y + dim[1] * z),
-                    (x + 1) + dim[0] * (y + dim[1] * (z + 1)),
-                    x + dim[0] * (y + dim[1] * (z + 1)),
-                    x + dim[0] * ((y + 1) + dim[1] * z),
-                    (x + 1) + dim[0] * ((y + 1) + dim[1] * z),
-                    (x + 1) + dim[0] * ((y + 1) + dim[1] * (z + 1)),
-                    x + dim[0] * ((y + 1) + dim[1] * (z + 1))
-                };
-
-                // Alternate the pattern so the edges line up on the sides of each voxel
-                if ((z % 2 ^ x % 2) ^ y % 2)
-                {
-                    indices.push_back(Vec4i(cubeIndices[0], cubeIndices[7], cubeIndices[5], cubeIndices[4]));
-                    indices.push_back(Vec4i(cubeIndices[3], cubeIndices[7], cubeIndices[2], cubeIndices[0]));
-                    indices.push_back(Vec4i(cubeIndices[2], cubeIndices[7], cubeIndices[5], cubeIndices[0]));
-                    indices.push_back(Vec4i(cubeIndices[1], cubeIndices[2], cubeIndices[0], cubeIndices[5]));
-                    indices.push_back(Vec4i(cubeIndices[2], cubeIndices[6], cubeIndices[7], cubeIndices[5]));
-                }
-                else
-                {
-                    indices.push_back(Vec4i(cubeIndices[3], cubeIndices[7], cubeIndices[6], cubeIndices[4]));
-                    indices.push_back(Vec4i(cubeIndices[1], cubeIndices[3], cubeIndices[6], cubeIndices[4]));
-                    indices.push_back(Vec4i(cubeIndices[3], cubeIndices[6], cubeIndices[2], cubeIndices[1]));
-                    indices.push_back(Vec4i(cubeIndices[1], cubeIndices[6], cubeIndices[5], cubeIndices[4]));
-                    indices.push_back(Vec4i(cubeIndices[0], cubeIndices[3], cubeIndices[1], cubeIndices[4]));
-                }
-            }
-        }
-    }
-
-    imstkNew<VecDataArray<float, 2>> uvCoordsPtr(dim[0] * dim[1] * dim[2]);
-    VecDataArray<float, 2>&          uvCoords = *uvCoordsPtr.get();
-    for (int z = 0; z < dim[2]; z++)
-    {
-        for (int y = 0; y < dim[1]; y++)
-        {
-            for (int x = 0; x < dim[0]; x++)
-            {
-                uvCoords[x + dim[0] * (y + dim[1] * z)] = Vec2f(static_cast<float>(x) / dim[0], static_cast<float>(z) / dim[2]) * 3.0;
-            }
-        }
-    }
-
-    // Ensure correct windings
-    for (int i = 0; i < indices.size(); i++)
-    {
-        if (tetVolume(vertices[indices[i][0]], vertices[indices[i][1]], vertices[indices[i][2]], vertices[indices[i][3]]) < 0.0)
-        {
-            std::swap(indices[i][0], indices[i][2]);
-        }
-    }
-
-    prismMesh->initialize(verticesPtr, indicesPtr);
-    prismMesh->setVertexTCoords("uvs", uvCoordsPtr);
-
-    return prismMesh;
-}
-
 static std::shared_ptr<PbdObject>
 makePbdObjSurface(const std::string& name,
                   const Vec3d&       size,
@@ -155,7 +60,7 @@ makePbdObjSurface(const std::string& name,
     imstkNew<PbdObject> prismObj(name);
 
     // Setup the Geometry
-    std::shared_ptr<TetrahedralMesh> prismMesh = makeTetGrid(size, dim, center);
+    std::shared_ptr<TetrahedralMesh> prismMesh = GeometryUtils::toTetGrid(center, size, dim);
     std::shared_ptr<SurfaceMesh>     surfMesh  = prismMesh->extractSurfaceMesh();
 
     // Setup the Parameters
@@ -321,7 +226,7 @@ main()
 
         imstkNew<RigidObjectController> controller(toolObj, hapticDeviceClient);
         controller->setTranslationScaling(0.05);
-        controller->setLinearKs(10000.0);
+        controller->setLinearKs(5000.0);
         controller->setAngularKs(1000.0);
         //controller->setAngularKs(0.0);
         controller->setUseCritDamping(true);
