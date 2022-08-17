5c777c529aa047287bdc28cc768d4fbfbf2416bf
Andrew Wilson <andx_roo@live.com>
2022-08-17 02:17:17 -0400

    ENH: Update PbdTissueContactExample to use a two-way proper compliance interaction with new xpbd simualted rigid bodies

--- Examples/PBD/PBDTissueContact/PBDTissueContactExample.cpp
+++ Examples/PBD/PBDTissueContact/PBDTissueContactExample.cpp
@@ -5,52 +5,33 @@
 */
 
 #include "imstkCamera.h"
-#include "imstkClosedSurfaceMeshToMeshCD.h"
 #include "imstkDirectionalLight.h"
 #include "imstkGeometryUtilities.h"
 #include "imstkImageData.h"
 #include "imstkKeyboardDeviceClient.h"
 #include "imstkKeyboardSceneControl.h"
-#include "imstkLineMesh.h"
 #include "imstkMeshIO.h"
 #include "imstkMouseDeviceClient.h"
 #include "imstkMouseSceneControl.h"
-#include "imstkNew.h"
+#include "imstkPbdCollisionHandling.h"
 #include "imstkPbdModel.h"
 #include "imstkPbdModelConfig.h"
 #include "imstkPbdObject.h"
+#include "imstkPbdObjectCollision.h"
+#include "imstkPbdObjectController.h"
 #include "imstkPointwiseMap.h"
-#include "imstkRbdConstraint.h"
 #include "imstkRenderMaterial.h"
-#include "imstkRigidBodyModel2.h"
-#include "imstkRigidObject2.h"
 #include "imstkScene.h"
 #include "imstkSceneManager.h"
 #include "imstkSimulationManager.h"
-#include "imstkSurfaceMesh.h"
-#include "imstkTetrahedralMesh.h"
-#include "imstkVisualModel.h"
+#include "imstkTextVisualModel.h"
 #include "imstkVTKViewer.h"
-#include "imstkSphere.h"
-
-// If two-way coupling is used haptic forces can be felt when the tool
-// hits the tissue
-//#define TWOWAY_COUPLING
-
-// Whether to use FEM or volume+distance constraints
-#define USE_FEM
-
-#ifdef TWOWAY_COUPLING
-#include "imstkPbdRigidObjectCollision.h"
-#else
-#include "imstkPbdObjectCollision.h"
-#endif
 
 #ifdef iMSTK_USE_OPENHAPTICS
 #include "imstkHapticDeviceManager.h"
 #include "imstkHapticDeviceClient.h"
-#include "imstkRigidObjectController.h"
 #else
+#include "imstkDummyClient.h"
 #include "imstkMouseDeviceClient.h"
 #endif
 
@@ -69,8 +50,8 @@ setSphereTexCoords(std::shared_ptr<SurfaceMesh> surfMesh, const double uvScale)
 
     const double radius = (size * 0.5).norm();
 
-    imstkNew<VecDataArray<float, 2>> uvCoordsPtr(surfMesh->getNumVertices());
-    VecDataArray<float, 2>&          uvCoords = *uvCoordsPtr.get();
+    auto                    uvCoordsPtr = std::make_shared<VecDataArray<float, 2>>(surfMesh->getNumVertices());
+    VecDataArray<float, 2>& uvCoords    = *uvCoordsPtr.get();
     for (int i = 0; i < surfMesh->getNumVertices(); i++)
     {
         Vec3d vertex = surfMesh->getVertexPosition(i) - center;
@@ -84,45 +65,25 @@ setSphereTexCoords(std::shared_ptr<SurfaceMesh> surfMesh, const double uvScale)
 }
 
 ///
-/// \brief Creates tissue object
+/// \brief Creates pbd simulated tissue object
 /// \param name
 /// \param physical dimension of tissue
 /// \param dimensions of tetrahedral grid used for tissue
 /// \param center of tissue block
+/// \param model dynamical model the tissue should use
 ///
 static std::shared_ptr<PbdObject>
 makeTissueObj(const std::string& name,
-              const Vec3d& size, const Vec3i& dim, const Vec3d& center)
+              const Vec3d& size, const Vec3i& dim, const Vec3d& center,
+              std::shared_ptr<PbdModel> model)
 {
     // Setup the Geometry
     std::shared_ptr<TetrahedralMesh> tissueMesh = GeometryUtils::toTetGrid(center, size, dim);
     std::shared_ptr<SurfaceMesh>     surfMesh   = tissueMesh->extractSurfaceMesh();
     setSphereTexCoords(surfMesh, 4.0);
 
-    // Setup the Parameters
-    imstkNew<PbdModelConfig> pbdParams;
-#ifdef USE_FEM
-    // Use FEMTet constraints
-    pbdParams->m_femParams->m_YoungModulus = 5.0;
-    pbdParams->m_femParams->m_PoissonRatio = 0.4;
-    pbdParams->enableFemConstraint(PbdFemConstraint::MaterialType::NeoHookean);
-#else
-    // Use volume+distance constraints, worse results. More performant (can use larger mesh)
-    pbdParams->enableConstraint(PbdConstraint::Type::Volume, 0.9);
-    pbdParams->enableConstraint(PbdConstraint::Type::Distance, 0.95);
-#endif
-    pbdParams->m_doPartitioning = true;
-    pbdParams->m_gravity    = Vec3d(0.0, 0.0, 0.0);
-    pbdParams->m_dt         = 0.05;
-    pbdParams->m_iterations = 5;
-    pbdParams->m_linearDampingCoeff = 0.1;
-
-    // Setup the Model
-    imstkNew<PbdModel> pbdModel;
-    pbdModel->configure(pbdParams);
-
     // Setup the material
-    imstkNew<RenderMaterial> material;
+    auto material = std::make_shared<RenderMaterial>();
     material->setShadingModel(RenderMaterial::ShadingModel::PBR);
     auto diffuseTex = MeshIO::read<ImageData>(iMSTK_DATA_ROOT "/textures/fleshDiffuse.jpg");
     material->addTexture(std::make_shared<Texture>(diffuseTex, Texture::Type::Diffuse));
@@ -132,24 +93,24 @@ makeTissueObj(const std::string& name,
     material->addTexture(std::make_shared<Texture>(ormTex, Texture::Type::ORM));
 
     // Add a visual model to render the surface of the tet mesh
-    imstkNew<VisualModel> visualModel;
+    auto visualModel = std::make_shared<VisualModel>();
     visualModel->setGeometry(surfMesh);
     visualModel->setRenderMaterial(material);
 
     // Add a visual model to render the normals of the surface
-    imstkNew<VisualModel> normalsVisualModel;
+    auto normalsVisualModel = std::make_shared<VisualModel>();
     normalsVisualModel->setGeometry(surfMesh);
     normalsVisualModel->getRenderMaterial()->setDisplayMode(RenderMaterial::DisplayMode::SurfaceNormals);
     normalsVisualModel->getRenderMaterial()->setPointSize(0.5);
 
     // Setup the Object
-    imstkNew<PbdObject> tissueObj(name);
+    auto tissueObj = std::make_shared<PbdObject>(name);
     tissueObj->addVisualModel(visualModel);
     tissueObj->addVisualModel(normalsVisualModel);
     tissueObj->setPhysicsGeometry(tissueMesh);
     tissueObj->setCollidingGeometry(surfMesh);
     tissueObj->setPhysicsToCollidingMap(std::make_shared<PointwiseMap>(tissueMesh, surfMesh));
-    tissueObj->setDynamicalModel(pbdModel);
+    tissueObj->setDynamicalModel(model);
     tissueObj->getPbdBody()->uniformMassValue = 0.05;
     // Fix the borders
     for (int z = 0; z < dim[2]; z++)
@@ -165,56 +126,65 @@ makeTissueObj(const std::string& name,
             }
         }
     }
+    model->getConfig()->m_femParams->m_YoungModulus = 50.0;
+    model->getConfig()->m_femParams->m_PoissonRatio = 0.4;
+    model->getConfig()->enableFemConstraint(PbdFemConstraint::MaterialType::NeoHookean);
+    model->getConfig()->setBodyDamping(tissueObj->getPbdBody()->bodyHandle, 0.1);
 
     return tissueObj;
 }
 
-static std::shared_ptr<RigidObject2>
-makeToolObj()
+///
+/// \brief Create pbd simulated rigid tool object
+/// \param model dynamical model the tool should use
+///
+static std::shared_ptr<PbdObject>
+makeToolObj(std::shared_ptr<PbdModel> model)
 {
-    imstkNew<LineMesh>                toolGeometry;
-    imstkNew<VecDataArray<double, 3>> verticesPtr(2);
-    (*verticesPtr)[0] = Vec3d(0.0, 0.0, 0.0);
-    (*verticesPtr)[1] = Vec3d(0.0, 2.0, 0.0);
-    imstkNew<VecDataArray<int, 2>> indicesPtr(1);
-    (*indicesPtr)[0] = Vec2i(0, 1);
-    toolGeometry->initialize(verticesPtr, indicesPtr);
+    auto                    toolGeometry = std::make_shared<LineMesh>();
+    VecDataArray<double, 3> vertices     = { Vec3d(0.0, 0.0, 0.0), Vec3d(0.0, 2.0, 0.0) };
+    VecDataArray<int, 2>    indices      = { Vec2i(0, 1) };
+    toolGeometry->initialize(std::make_shared<VecDataArray<double, 3>>(vertices),
+        std::make_shared<VecDataArray<int, 2>>(indices));
 
-    imstkNew<RigidObject2> toolObj("Tool");
+    auto toolObj = std::make_shared<PbdObject>("Tool");
     toolObj->setVisualGeometry(toolGeometry);
     toolObj->setCollidingGeometry(toolGeometry);
     toolObj->setPhysicsGeometry(toolGeometry);
+    toolObj->setDynamicalModel(model);
     toolObj->getVisualModel(0)->getRenderMaterial()->setColor(Color::Blue);
     toolObj->getVisualModel(0)->getRenderMaterial()->setDisplayMode(RenderMaterial::DisplayMode::Wireframe);
     toolObj->getVisualModel(0)->getRenderMaterial()->setBackFaceCulling(false);
     toolObj->getVisualModel(0)->getRenderMaterial()->setLineWidth(10.0);
 
-    std::shared_ptr<RigidBodyModel2> rbdModel = std::make_shared<RigidBodyModel2>();
-    rbdModel->getConfig()->m_gravity = Vec3d::Zero();
-    // Due to the lack of a compliance model between PBD-RBD a complete solve is performed
-    // resulting in the tool moving completely out of the PBD tissue. Meaning it is non-deformable
-    // or nearly.
-    // This can be cheated by not completely solving the RBD by lowering step size & iterations
-    rbdModel->getConfig()->m_maxNumIterations       = 7;
-    rbdModel->getConfig()->m_velocityDamping        = 0.95;
-    rbdModel->getConfig()->m_angularVelocityDamping = 1.0;
-    rbdModel->getConfig()->m_maxNumConstraints      = 40;
-    toolObj->setDynamicalModel(rbdModel);
+    model->getConfig()->setBodyDamping(toolObj->getPbdBody()->bodyHandle, 0.05, 0.0);
 
-    toolObj->getRigidBody()->m_mass = 0.2;
-    toolObj->getRigidBody()->m_intertiaTensor = Mat3d::Identity() * 10000.0;
-#ifdef iMSTK_USE_OPENHAPTICS
-    toolObj->getRigidBody()->m_initPos = Vec3d(0.0, 0.8, 0.0);
-#else
-    toolObj->getRigidBody()->m_initPos = Vec3d(0.0, 0.0, 0.0);
-#endif
+    toolObj->getPbdBody()->setRigid(Vec3d(0.0, 0.8, 0.0), 0.2,
+        Quatd::Identity(), Mat3d::Identity() * 10000.0);
 
     return toolObj;
 }
 
 ///
-/// \brief This example demonstrates collision interaction with a 3d pbd
-/// simulated tissue (tetrahedral)
+/// \brief Creates a text object with text in the top right
+///
+static std::shared_ptr<SceneObject>
+makeTextObj()
+{
+    auto txtVisualModel = std::make_shared<TextVisualModel>();
+    txtVisualModel->setText("Device Force: 0N\nContact Force: 0N");
+    txtVisualModel->setPosition(TextVisualModel::DisplayPosition::UpperRight);
+    auto obj = std::make_shared<SceneObject>();
+    obj->addVisualModel(txtVisualModel);
+    return obj;
+}
+
+///
+/// \brief This example demonstrates two-way collision interaction with a 3d
+/// xpbd simulated tool and tissue (tetrahedral). With proper compliance.
+///
+/// If imstk is built with haptics a force is rendered. Otherwise mouse controls
+/// are used. This example is currently not too scale, a force scaling is used.
 ///
 int
 main()
@@ -223,109 +193,110 @@ main()
     Logger::startLogger();
 
     // Setup the scene
-    imstkNew<Scene> scene("PBDTissueContact");
+    auto scene = std::make_shared<Scene>("PbdTissueContact");
     scene->getActiveCamera()->setPosition(0.12, 4.51, 16.51);
     scene->getActiveCamera()->setFocalPoint(0.0, 0.0, 0.0);
     scene->getActiveCamera()->setViewUp(0.0, 0.96, -0.28);
 
+    // Setup the Model/System
+    auto pbdModel = std::make_shared<PbdModel>();
+    pbdModel->getConfig()->m_doPartitioning = false;
+    pbdModel->getConfig()->m_gravity    = Vec3d(0.0, 0.0, 0.0);
+    pbdModel->getConfig()->m_dt         = 0.05;
+    pbdModel->getConfig()->m_iterations = 5;
+
     // Setup a tissue
     std::shared_ptr<PbdObject> tissueObj = makeTissueObj("Tissue",
-        Vec3d(8.0, 2.0, 8.0), Vec3i(6, 5, 6), Vec3d(0.0, -1.0, 0.0));
+        Vec3d(8.0, 2.0, 8.0), Vec3i(6, 5, 6), Vec3d(0.0, -1.0, 0.0), pbdModel);
     scene->addSceneObject(tissueObj);
 
-    std::shared_ptr<RigidObject2> toolObj = makeToolObj();
+    // Setup a tool
+    std::shared_ptr<PbdObject> toolObj = makeToolObj(pbdModel);
     scene->addSceneObject(toolObj);
 
-#ifdef TWOWAY_COUPLING
+    // Setup a text to display forces
+    std::shared_ptr<SceneObject> txtObj = makeTextObj();
+    scene->addSceneObject(txtObj);
+
     // With PbdRigidObjectCollision we have Pbd-Rigid coupling
     // The toolObj responds to the tissue (the tool is pushed partly out of the way of the tissue whilst the
     // tissue deforms)
-    auto interaction = std::make_shared<PbdRigidObjectCollision>(tissueObj, toolObj, "ClosedSurfaceMeshToMeshCD");
-    std::dynamic_pointer_cast<ClosedSurfaceMeshToMeshCD>(interaction->getCollisionDetection())->setGenerateEdgeEdgeContacts(true);
-    scene->addInteraction(interaction);
-#else
-    // With PbdObjectCollision we only have one-way coupling
-    // The toolObj does not respond to the tissue (only the tissueObj responds to the tool, moving out of the way)
-    auto interaction = std::make_shared<PbdObjectCollision>(tissueObj, toolObj, "ClosedSurfaceMeshToMeshCD");
-    std::dynamic_pointer_cast<ClosedSurfaceMeshToMeshCD>(interaction->getCollisionDetection())->setGenerateEdgeEdgeContacts(true);
-    scene->addInteraction(interaction);
-#endif
+    auto collision = std::make_shared<PbdObjectCollision>(tissueObj, toolObj, "ClosedSurfaceMeshToMeshCD");
+    //std::dynamic_pointer_cast<ClosedSurfaceMeshToMeshCD>(collision->getCollisionDetection())->setGenerateEdgeEdgeContacts(true);
+    scene->addInteraction(collision);
 
     // Light
-    imstkNew<DirectionalLight> light;
+    auto light = std::make_shared<DirectionalLight>();
     light->setFocalPoint(Vec3d(5.0, -8.0, -5.0));
-    light->setIntensity(1);
+    light->setIntensity(1.0);
     scene->addLight("Light", light);
 
     // Run the simulation
     {
         // Setup a viewer to render
-        imstkNew<VTKViewer> viewer;
+        auto viewer = std::make_shared<VTKViewer>();
         viewer->setActiveScene(scene);
         viewer->setVtkLoggerMode(VTKViewer::VTKLoggerMode::MUTE);
 
         // Setup a scene manager to advance the scene
-        imstkNew<SceneManager> sceneManager;
+        auto sceneManager = std::make_shared<SceneManager>();
         sceneManager->setActiveScene(scene);
         sceneManager->pause(); // Start simulation paused
 
-        imstkNew<SimulationManager> driver;
+        auto driver = std::make_shared<SimulationManager>();
         driver->addModule(viewer);
         driver->addModule(sceneManager);
         driver->setDesiredDt(0.001);
 
+        auto controller = std::make_shared<PbdObjectController>();
 #ifdef iMSTK_USE_OPENHAPTICS
-        imstkNew<HapticDeviceManager> hapticManager;
+        auto hapticManager = std::make_shared<HapticDeviceManager>();
         hapticManager->setSleepDelay(1.0); // Delay for 1ms (haptics thread is limited to max 1000hz)
-        std::shared_ptr<HapticDeviceClient> hapticDeviceClient = hapticManager->makeDeviceClient();
+        std::shared_ptr<HapticDeviceClient> deviceClient = hapticManager->makeDeviceClient();
         driver->addModule(hapticManager);
 
-        imstkNew<RigidObjectController> controller;
-        controller->setControlledObject(toolObj);
-        controller->setDevice(hapticDeviceClient);
         controller->setTranslationScaling(0.05);
+#else
+        auto deviceClient = std::make_shared<DummyClient>();
+        connect<Event>(sceneManager, &SceneManager::postUpdate, [&](Event*)
+            {
+                const Vec2d mousePos = viewer->getMouseDevice()->getPos();
+                const Vec3d worldPos = Vec3d(mousePos[0] - 0.5, mousePos[1] - 0.5, 0.0) * 10.0;
+
+                deviceClient->setPosition(worldPos);
+            });
+
+        controller->setTranslationScaling(1.0);
+#endif
+        controller->setControlledObject(toolObj);
+        controller->setDevice(deviceClient);
         controller->setLinearKs(5000.0);
         controller->setAngularKs(10000000.0);
         controller->setUseCritDamping(true);
         controller->setForceScaling(0.0025);
         controller->setUseForceSmoothening(true);
         scene->addControl(controller);
-#else
-        connect<Event>(sceneManager, &SceneManager::postUpdate, [&](Event*)
-            {
-                const Vec2d mousePos = viewer->getMouseDevice()->getPos();
-                const Vec3d worldPos = Vec3d(mousePos[0] - 0.5, mousePos[1] - 0.5, 0.0) * 10.0;
-
-                const Vec3d fS = (worldPos - toolObj->getRigidBody()->getPosition()) * 1000.0; // Spring force
-                const Vec3d fD = -toolObj->getRigidBody()->getVelocity() * 100.0;              // Spring damping
-
-                (*toolObj->getRigidBody()->m_force) += (fS + fD);
-            });
-#endif
-
-        // Toggle collision interaction in scene with key y
-        connect<KeyEvent>(viewer->getKeyboardDevice(), &KeyboardDeviceClient::keyPress,
-            [&](KeyEvent* e)
-            {
-                if (e->m_key == 'y')
-                {
-                    if (scene->getSceneObject(interaction->getName()) != nullptr)
-                    {
-                        scene->removeSceneObject(interaction);
-                    }
-                    else
-                    {
-                        scene->addInteraction(interaction);
-                    }
-                    scene->buildTaskGraph();
-                    scene->initTaskGraph();
-                }
-            });
 
         connect<Event>(sceneManager, &SceneManager::postUpdate, [&](Event*)
             {
                 // Keep the tool moving in real time
-                toolObj->getRigidBodyModel2()->getConfig()->m_dt = sceneManager->getDt();
+                pbdModel->getConfig()->m_dt = sceneManager->getDt();
+
+                // Assume first collision constraint
+                double contactForceMag = 0.0;
+                auto pbdCH = std::dynamic_pointer_cast<PbdCollisionHandling>(collision->getCollisionHandlingAB());
+                const std::vector<PbdConstraint*>& collisionConstraints = pbdCH->getConstraints();
+                if (collisionConstraints.size() > 0)
+                {
+                    // Multiply with gradient for direction
+                    contactForceMag  = std::abs(collisionConstraints[0]->getForce(pbdModel->getConfig()->m_dt));
+                    contactForceMag *= controller->getForceScaling(); // Scale to bring in device space
+                }
+
+                // Display spring and contact force
+                const std::string forceTxt = "Device Force: " + std::to_string(controller->getDeviceForce().norm()) + "N\n" +
+                                             "Scaled Contact Force: " + std::to_string(contactForceMag) + 'N';
+                std::dynamic_pointer_cast<TextVisualModel>(txtObj->getVisualModel(0))->setText(forceTxt);
         });
 
         // Add mouse and keyboard controls to the viewer
