21408b6e48d02ff1c229ddb8ce2da727f4d874dd
Shreeraj Jadhav <shreerajjadhav@gmail.com>
2022-11-23 13:29:43 -0500

    ENH: Add new component imstk::Collider

---
+++ Source/SceneEntities/Components/imstkCollider.h
@@ -0,0 +1,61 @@
+/*
+** This file is part of the Interactive Medical Simulation Toolkit (iMSTK)
+** iMSTK is distributed under the Apache License, Version 2.0.
+** See accompanying NOTICE for details.
+*/
+
+#pragma once
+
+#include "imstkComponent.h"
+#include "imstkEventObject.h"
+
+#include <memory>
+#include <string>
+
+namespace imstk
+{
+class Geometry;
+class GeometryMap;
+
+///
+/// \class Collider
+///
+/// \brief Contains geometry for collision.
+///
+class Collider : public Component
+{
+public:
+    Collider(const std::string& name = "Collider")
+      : Component(name)
+    {
+    }
+
+    ~Collider() override = default;
+
+    ///
+    /// \brief Get/Set geometry
+    ///@{
+    std::shared_ptr<Geometry> getGeometry() const { return m_geometry; }
+    void setGeometry(std::shared_ptr<Geometry> geometry) { m_geometry = geometry; }
+    ///@}
+
+    ///
+    /// \brief
+    ///@{
+    std::shared_ptr<GeometryMap> getCollidingToVisualMap() const { return m_collidingToVisualMap; }
+    void setCollidingToVisualMap(std::shared_ptr<GeometryMap> map) { m_collidingToVisualMap = map; }
+    ///@}
+
+    ///
+    /// \brief Get/Set name
+    ///@{
+    const std::string& getName() const { return m_name; }
+    void setName(std::string name) { m_name = name; }
+    ///@}
+
+    static std::shared_ptr<Geometry> getCollidingGeometryFromEntity(Entity* entity);
+
+protected:
+    std::shared_ptr<Geometry> m_geometry; ///< Geometry for collisions
+};
+} // namespace imstk
