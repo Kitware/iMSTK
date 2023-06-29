2333a994baadc782be03bdfc4cc65a89840a87f6
Jacob Moore <jacob.moore@kitware.com>
2023-06-29 12:43:18 -0400

    BUG: Adding debugging and fix crash
    (cherry picked from commit 3d75dc942025ec95b253624b574f05c06af686d9)

--- Source/Devices/imstkOpenHapticDeviceManager.h
+++ Source/Devices/imstkOpenHapticDeviceManager.h
@@ -14,39 +14,39 @@
 
 namespace imstk
 {
-class OpenHapticDeviceClient;
-
-///
-/// \class OpenHapticDeviceManager
-///
-/// \brief Devices manager using HDAPI
-/// \todo add the frame rate option for the servo loop
-///
-class OpenHapticDeviceManager : public DeviceManager
-{
-public:
-    OpenHapticDeviceManager();
-
-    ~OpenHapticDeviceManager() override = default;
-
-    IMSTK_TYPE_NAME(OpenHapticDeviceManager)
+    class OpenHapticDeviceClient;
 
     ///
-    /// \brief Create a haptic device client and add it to the internal list
-    /// \param Device name or use empty string for default device
+    /// \class OpenHapticDeviceManager
     ///
-    std::shared_ptr<DeviceClient> makeDeviceClient(std::string name = "") override;
+    /// \brief Devices manager using HDAPI
+    /// \todo add the frame rate option for the servo loop
+    ///
+    class OpenHapticDeviceManager : public DeviceManager
+    {
+    public:
+        OpenHapticDeviceManager();
 
-protected:
+        ~OpenHapticDeviceManager() override = default;
 
-    bool initModule() override;
+        IMSTK_TYPE_NAME(OpenHapticDeviceManager)
 
-    void updateModule() override;
+            ///
+            /// \brief Create a haptic device client and add it to the internal list
+            /// \param Device name or use empty string for default device
+            ///
+            std::shared_ptr<DeviceClient> makeDeviceClient(std::string name = "") override;
 
-    void uninitModule() override;
+    protected:
 
-private:
-    class OpenHapticDeviceManagerImpl;
-    std::unique_ptr<OpenHapticDeviceManagerImpl> m_impl;
-};
+        bool initModule() override;
+
+        void updateModule() override;
+
+        void uninitModule() override;
+
+    private:
+        class OpenHapticDeviceManagerImpl;
+        std::unique_ptr<OpenHapticDeviceManagerImpl> m_impl;
+    };
 } // namespace imstk
