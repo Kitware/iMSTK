a27689827db6669b2837e938034f2e6fe7fb8961
Harald Scheirich <harald.scheirich@kitware.com>
2023-10-18 08:32:17 -0400

    STYLE: Uncrustify

--- Source/Devices/imstkDummyClient.cpp
+++ Source/Devices/imstkDummyClient.cpp
@@ -9,45 +9,46 @@
 
 namespace imstk
 {
+void
+DummyClient::init(const unsigned int numButtons /*= 0*/)
+{
+    for (unsigned int i = 0; i < numButtons; i++)
+    {
+        m_buttons[i] = false;
+    }
+}
 
-	void DummyClient::init(const unsigned int numButtons /*= 0*/)
-	{
-		for (unsigned int i = 0; i < numButtons; i++)
-		{
-			m_buttons[i] = false;
-		}
-	}
+void
+DummyClient::setPosition(const Vec3d& pos)
+{
+    m_transformLock.lock();
+    m_position = pos;
+    m_transformLock.unlock();
+}
 
-	void DummyClient::setPosition(const Vec3d& pos)
-	{
+void
+DummyClient::setVelocity(const Vec3d& vel)
+{
+    m_transformLock.lock();
+    m_velocity = vel;
+    m_transformLock.unlock();
+}
 
-		m_transformLock.lock();
-		m_position = pos;
-		m_transformLock.unlock();
-	}
+void
+DummyClient::setOrientation(const Quatd& orient)
+{
+    m_transformLock.lock();
+    m_orientation = orient;
+    m_transformLock.unlock();
+}
 
-	void DummyClient::setVelocity(const Vec3d& vel)
-	{
-
-		m_transformLock.lock();
-		m_velocity = vel;
-		m_transformLock.unlock();
-	}
-
-	void DummyClient::setOrientation(const Quatd& orient)
-	{
-
-		m_transformLock.lock();
-		m_orientation = orient;
-		m_transformLock.unlock();
-	}
-
-	void DummyClient::setOrientation(double* transform)
-	{
-		m_transformLock.lock();
-		m_orientation = (Eigen::Affine3d(Eigen::Matrix4d(transform))).rotation();
-		m_transformLock.unlock();
-	}
+void
+DummyClient::setOrientation(double* transform)
+{
+    m_transformLock.lock();
+    m_orientation = (Eigen::Affine3d(Eigen::Matrix4d(transform))).rotation();
+    m_transformLock.unlock();
+}
 
 void
 DummyClient::setButton(const unsigned int buttonId, const bool buttonStatus)
